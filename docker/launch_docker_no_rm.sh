bc802561544cc8d3cf6a2042e81467d432887e59
iandix <ianaguidinix@gmail.com>
2018-07-08 23:33:42 -0300

    Added docker related files

---
+++ docker/launch_docker_no_rm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+xhost +local:root
+
+IMAGE=$1
+nvidia-docker run -it \
+--env="DISPLAY"  \
+--env="QT_X11_NO_MITSHM=1"  \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--workdir="/home/$USER" \
+--volume="$HOME/host_docker:/home/user/host_docker" \
+--mount source=learn-ros,target="/home/$USER/dev" \
+-e LOCAL_USER_ID=`id -u $USER` \
+-e LOCAL_GROUP_ID=`id -g $USER` \
+-e LOCAL_GROUP_NAME=`id -gn $USER` \
+ $IMAGE
+
+xhost -local:root
